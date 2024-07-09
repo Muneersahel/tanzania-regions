556df653606840be1fbd6b30496ef6dced2b73ed
Muneersahel <saidmunir73@gmail.com>
2024-07-09 22:36:02 +0300

    chore: add neccessary commits for available functions

--- libs/regions/src/lib/regions.ts
+++ libs/regions/src/lib/regions.ts
@@ -2,30 +2,45 @@ import data from '../data/tanzania.json';
 
 const country: Country = data;
 
+/**
+ *
+ * @returns a list of country regions (Tanzania regions)
+ */
 export function regions() {
   return country;
 }
 
+/**
+ *
+ * @param region string
+ * @returns region with the list of districts
+ */
 export function districts(region: string) {
-  const requiredRegion: Region | undefined = country.regions.find(
+  const foundRegion: Region | undefined = country.regions.find(
     (r) => r.slug === region,
   );
-  if (requiredRegion) return requiredRegion;
+  if (foundRegion) return foundRegion;
   return { error: 'Could not find region with the slug specified' };
 }
 
+/**
+ *
+ * @param region string
+ * @param district string
+ * @returns a district with the list of wards
+ */
 export function wards(region: string, district: string) {
-  const requiredRegion: Region | undefined = country.regions.find(
+  const foundRegion: Region | undefined = country.regions.find(
     (r) => r.slug === region,
   );
-  if (!requiredRegion) {
+  if (!foundRegion) {
     return { error: 'Could not find region with the slug specified' };
   }
 
-  const requiredDistrict: District | undefined = requiredRegion.districts.find(
+  const foundDistrict: District | undefined = foundRegion.districts.find(
     (r) => r.slug === district,
   );
-  if (requiredDistrict) return requiredDistrict;
+  if (foundDistrict) return foundDistrict;
   return { error: 'Could not find district with the slug specified' };
 }
 
